1ded59d3e05ed8c3fa610fabc8f74474027b56fa
0xjojikun <jojikun@intuition.systems>
2023-10-16 20:49:09 -0600

    Update query routes based on API changes

--- README.md
+++ README.md
@@ -3,31 +3,39 @@
 This guide covers authentication, identity creation, claim attestation, and querying identities & claims in TypeScript. By the end, you'll grasp the core functionalities and be ready for advanced examples.
 
 ## Prerequisites
+
 - **Node.js**: Ensure Node.js (v16.14.0 or newer) is installed. Update if using an older version.
 
 ## Setup
+
 1. **Clone the Repository**:
+
    ```bash
    git clone git@github.com:0xIntuition/speedrun-examples.git
    ```
 
 2. **Install Dependencies**:
+
    ```bash
    npm install
-   # or 
+   # or
    yarn install
-   # or 
+   # or
    pnpm install
    ```
 
 3. **Environment Configuration**:
    Create a `.env` file and set the following:
+
    ```bash
    API_KEY=YOUR_API_KEY
    PRIVATE_KEY=0xYOUR_PRIVATE_KEY
+   API_URL=https://api.intuition.cafe
    ```
+
    - `API_KEY`: Essential for all API requests.
    - `PRIVATE_KEY`: Linked to the API key wallet, used for message signing and Ceramic Network writes. Append 0x to the start of your private key.
+   - `API_URL`: Optional. Set to `https://api.intuition.cafe` by default.
 
    **Note: This is a private key, not a seed phrase.**
 
@@ -38,6 +46,7 @@ First, replace the `subject_display_name` and `subject_description` variables in
 Next, replace the `object_display_name` and `object_description` variables in `src/speedrun.ts` in the main function with your own values. These will be used to create an identity and claim.
 
 Execute the script to perform operations like DID session creation, identity and claim creation, and querying:
+
 ```bash
 npm run start
 # or
@@ -45,7 +54,9 @@ yarn start
 # or
 pnpm start
 ```
+
 Steps:
+
 1. Fetch and sign a message for DID session creation.
 2. Exchange the signed message for a DID session.
 3. Create identities.
@@ -55,6 +66,7 @@ Steps:
 7. Query for claims by creator wallet address.
 
 ## Resources
+
 - [Intuition Documentation](https://intuition.gitbook.io/alpha-api/): Dive into Intuition's core mechanics.
 - [Getting Started Guide](https://app.gitbook.com/o/xYyeoT5KBfRZxYH5NYQb/s/cVc9V0gt0E79kdhQIpdk/developer-docs/getting-started): A concise API guide for claim creation, attestation, and querying.
 - [Contact Us](https://discord.gg/0xintuition): Connect with the Intuition team for help.
--- speedrun.ts
+++ speedrun.ts
@@ -1,55 +1,64 @@
-import fetch from 'node-fetch';
-import { config } from 'dotenv'; 
+import fetch from 'node-fetch'
+import { config } from 'dotenv'
 import { SiweMessage } from '@didtools/cacao'
 import { privateKeyToAccount } from 'viem/accounts'
 config()
+
+const apiUrl = (process.env.API_URL as string) ?? 'https://api.intuition.cafe'
 
 // Authentication
 // Gets SIWE message to sign
 async function getMessage(apiKey: string, wallet: `0x${string}`): Promise<string | null> {
   try {
-    const response = await fetch('http://api.intuition.cafe/auth/message?wallet=' + encodeURIComponent(wallet as string), { headers: { 'Content-Type': 'application/json', 'x-api-key': apiKey } });
-    const data = await response.json() as { message: string};
+    const response = await fetch(`${apiUrl}/auth/message?wallet=${encodeURIComponent(wallet as string)}`, {
+      headers: { 'Content-Type': 'application/json', 'x-api-key': apiKey },
+    })
+    const data = (await response.json()) as { message: string }
     console.log(data)
-    return data.message;
+    return data.message
   } catch (error) {
-    console.error('Error fetching message:', error);
-    return null;
+    console.error('Error fetching message:', error)
+    return null
   }
 }
 // Signs message and returns message signature
 async function signMessage(message: string) {
-    console.log(message)
-    const msg = new SiweMessage(message)
-    const account = privateKeyToAccount(process.env.PRIVATE_KEY as `0x${string}`)
-    const signature = await account.signMessage({
-        message: msg.signMessage(),
-      })
-    console.log(signature)
-    return signature;
+  console.log(message)
+  const msg = new SiweMessage(message)
+  const account = privateKeyToAccount(process.env.PRIVATE_KEY as `0x${string}`)
+  const signature = await account.signMessage({
+    message: msg.signMessage(),
+  })
+  console.log(signature)
+  return signature
 }
 
 // Exchange a signed message for a DID Session
 async function getSession(msg: string, signature: string, wallet: `0x${string}`): Promise<string | null> {
-    const response = await fetch('http://api.intuition.cafe/auth/session?message=' + encodeURIComponent(msg) + '&signature=' + encodeURIComponent(signature) + '&wallet=' + encodeURIComponent(wallet as string), {
+  const response = await fetch(
+    `${apiUrl}/auth/session?message=${encodeURIComponent(msg)}&signature=${encodeURIComponent(
+      signature
+    )}&wallet=${encodeURIComponent(wallet as string)}`,
+    {
       headers: {
         'x-api-key': process.env.API_KEY as string,
-      }
-    });
-    if(!response.ok) {
-      const error = await response.json() as any
-      throw Error(error.message)
+      },
     }
-    const res = await response.json() as { session: string };
-    return res.session;
+  )
+  if (!response.ok) {
+    const error = (await response.json()) as any
+    throw Error(error.message)
+  }
+  const res = (await response.json()) as { session: string }
+  return res.session
 }
 // Identity Creation
 // Creates an identity given a displayName and description
 async function createIdentity(session: string, displayName: string, description: string): Promise<string> {
-  const response = await fetch('http://api.intuition.cafe/identity', {
+  const response = await fetch(`${apiUrl}/identity`, {
     method: 'POST',
     headers: {
-      'Authorization': 'Bearer ' + session,
+      Authorization: 'Bearer ' + session,
       'x-api-key': process.env.API_KEY as string,
       'Content-Type': 'application/json',
     },
@@ -57,45 +66,50 @@ async function createIdentity(session: string, displayName: string, description:
       display_name: displayName,
       description: description,
     }),
-  });
-  if(!response.ok) {
-    const error = await response.json() as any
+  })
+  if (!response.ok) {
+    const error = (await response.json()) as any
     throw Error(error.message)
   }
-  const res = await response.json() as any
+  const res = (await response.json()) as any
   return res.data.identity_id
 }
 // Claim Creation
 // Creates a claim given identity IDs for a subject, predicate and object.
-async function createClaim(session: string, subject_id: string, predicate_id: string, object_id: string): Promise<string> {
-  const response = await fetch('http://api.intuition.cafe/claim', {
+async function createClaim(
+  session: string,
+  subject_id: string,
+  predicate_id: string,
+  object_id: string
+): Promise<string> {
+  const response = await fetch(`${apiUrl}/claim`, {
     method: 'POST',
     headers: {
-      'Authorization': 'Bearer ' + session,
+      Authorization: 'Bearer ' + session,
       'x-api-key': process.env.API_KEY as string,
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
       subject_id: subject_id,
       predicate_id: predicate_id,
-      object_id: object_id
+      object_id: object_id,
     }),
-  });
-  if(!response.ok) {
-    const error = await response.json() as any
+  })
+  if (!response.ok) {
+    const error = (await response.json()) as any
     throw Error(error.message)
   }
-  const data = await response.json() as { data: { claim_id: string } };
-  console.log(data);
+  const data = (await response.json()) as { data: { claim_id: string } }
+  console.log(data)
   return data.data.claim_id
 }
 // Attesting
 // Attests to the specified Claim in the direction (true: for, false: against)
 async function attestToClaim(session: string, claim_id: string, direction: boolean): Promise<void> {
-  const response = await fetch('http://api.intuition.cafe/claim/attest', {
+  const response = await fetch(`${apiUrl}/claim/attest`, {
     method: 'POST',
     headers: {
-      'Authorization': 'Bearer ' + session,
+      Authorization: 'Bearer ' + session,
       'x-api-key': process.env.API_KEY as string,
       'Content-Type': 'application/json',
     },
@@ -103,64 +117,59 @@ async function attestToClaim(session: string, claim_id: string, direction: boole
       claim_id: claim_id,
       boolean: direction,
     }),
-  });
-  if(!response.ok) {
-    const error = await response.json() as any
+  })
+  if (!response.ok) {
+    const error = (await response.json()) as any
     throw Error(error.message)
-  } 
-  const data = await response.json();
-  console.log(data);
+  }
+  const data = await response.json()
+  console.log(data)
 }
 
 // Querying Identities
 // Attests to the specified Claim in the direction (true: for, false: against)
-async function queryIdentitiesByDisplayName(session: string, display_name_in: string, operator: string): Promise<any> {
-  const response = await fetch('http://api.intuition.cafe/query/identities', {
+async function queryIdentitiesByDisplayName(session: string, display_name: string): Promise<any> {
+  const response = await fetch(`${apiUrl}/query/identities`, {
     method: 'POST',
     headers: {
-      'Authorization': 'Bearer ' + session,
+      Authorization: 'Bearer ' + session,
       'x-api-key': process.env.API_KEY as string,
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      input: { 
-        display_name: { "value": display_name_in, op: operator } 
-      } 
+      display_names: [display_name],
     }),
-  });
-  if(!response.ok) {
-    const error = await response.json() as any
+  })
+  if (!response.ok) {
+    const error = (await response.json()) as any
     throw Error(error.message)
-  } 
-  const data = await response.json();
-  console.log(data);
-  return data as string
-}
-  
-// Querying claims created by a wallet address
-async function queryClaimsByCreator(session: string, wallet: string, operator: string): Promise<any> {
-  const response = await fetch('http://api.intuition.cafe/query/claims', {
-    method: 'POST',
-    headers: {
-      'Authorization': 'Bearer ' + session,
-      'x-api-key': process.env.API_KEY as string,
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      input: { 
-        creator: { "value": wallet, op: operator} 
-      } 
-    }),
-  });
-  if(!response.ok) {
-    const error = await response.json() as any
-    throw Error(error.message)
-  } 
-  const data = await response.json();
-  console.log(data);
+  }
+  const data = await response.json()
+  console.log(data)
   return data as string
 }
 
+// Querying claims created by a wallet address
+async function queryClaimsByCreator(session: string, wallet: string): Promise<any> {
+  const response = await fetch(`${apiUrl}/query/claims`, {
+    method: 'POST',
+    headers: {
+      Authorization: 'Bearer ' + session,
+      'x-api-key': process.env.API_KEY as string,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      creators: [wallet],
+    }),
+  })
+  if (!response.ok) {
+    const error = (await response.json()) as any
+    throw Error(error.message)
+  }
+  const data = await response.json()
+  console.log(data)
+  return data as string
+}
 
 async function main() {
   const wallet = privateKeyToAccount(process.env.PRIVATE_KEY as `0x${string}`)
@@ -180,17 +189,17 @@ async function main() {
    * The subject refers to the entity being described in the statement (e.g., Person, Place, Thing).
    * The predicate indicates a specific characteristic or relation of the Subject to an {Object}. In this claim, we'll be using the Identity ID with the predicate "isInteresting".
    * The object can be any reference of your choice, such as a Protocol, Article, Person, or any other noun.
-   * 
+   *
    * Task:
    * 1. Replace the subject, object display_name and description with your own values.
    * 2. Create an identity, will be used as the object in the claim {Subject}, {isInteresting}, {Object}
    * What is something you find interesting?
    */
-    const subject_display_name = "" // Replace me (e.g.: "Intuition", "Ethereum", "Chocolate Cake" etc.)
-    const subject_description = "" // Replace me 
-    const object_display_name = "" // Replace me (e.g.: "Protocol", "Startup", "Layer 2", "Pastry")
-    const object_description = "" // Replace me 
-  
+  const subject_display_name = '' // Replace me (e.g.: "Intuition", "Ethereum", "Chocolate Cake" etc.)
+  const subject_description = '' // Replace me
+  const object_display_name = '' // Replace me (e.g.: "Protocol", "Startup", "Layer 2", "Pastry")
+  const object_description = '' // Replace me
+
   // Create the subject. This resulting identity ID be used as the subject in the claim
   let subject_id = await createIdentity(session as string, subject_display_name, subject_description)
 
@@ -198,18 +207,23 @@ async function main() {
   let object_id = await createIdentity(session as string, object_display_name, object_description)
 
   // Create a Claim using the created Identities {YOUR_SUBJECT}-{isInteresting}-{YOUR_OBJECT}
-  let claim_id = await createClaim(session as string, subject_id, "3182bf90ef182429f0f5799b1679936cce5850feec6bbabc9de4936a4324de4c", object_id)
-  
+  let claim_id = await createClaim(
+    session as string,
+    subject_id,
+    '50ba5a74941e6456184e9fb1dbfec124eb3bd6e96fab0bdcbeac5de2da191180',
+    object_id
+  )
+
   // Attest to a Claim
-  // Attesting For a Claim is a way to endorse the Claim 
+  // Attesting For a Claim is a way to endorse the Claim
   await attestToClaim(session as string, claim_id, true)
-  
+
   // Query Identities & Claims
   // Query for identities by displayName
-  await queryIdentitiesByDisplayName(session as string, "Intuition", "like")
+  await queryIdentitiesByDisplayName(session as string, 'Intuition')
 
   // Query for Claims by Creator
-  await queryClaimsByCreator(session as string, "0xf37d1dd67b39fa291da3c67f5ed60fb77b1b92b7", "ilike")
+  await queryClaimsByCreator(session as string, '0xCeBcD6631947E46FEEFccA6A4D96468695cb8540')
 }
 
-main();
+main()
